b43020bdb00b82bdde33b5cce2d85b0dd67637ae
nebulousGirl <contact@nebulousweb.com>
2016-10-23 09:35:18 -0400

    Add tests for providers

---
+++ tests/LeagueProviderTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Laasti\Directions\Test;
+
+class LeagueProviderTest extends \PHPUnit_Framework_TestCase
+{
+    public function testAlias()
+    {
+        $container = new \League\Container\Container();
+        $container->add('config', [
+            'directions' => [
+                'default' => []
+            ]
+        ]);
+        $container->addServiceProvider('Laasti\Directions\Providers\LeagueDirectionsProvider');
+        $this->assertInstanceOf('Laasti\Directions\Router', $container->get('directions.routers.default'));
+        $this->assertInstanceOf('Laasti\Directions\Router', $container->get('Laasti\Directions\RouterInterface'));
+        
+    }
+}
